0ddfbc628ae0c604264deea819314393aab85e14
Richard Asiedu <rich.learnonline@gmail.com>
2021-05-08 08:26:25 -0700

    created the BMI v2.0

---
+++ day-3-bmi-2.0.py
@@ -0,0 +1,27 @@
+# Under 18.5 they are underweight
+# Over 18.5 but below 25 they have a normal weight
+# Over 25 but below 30 they are slightly overweight
+# Over 30 but below 35 they are obese
+# Above 35 they are clinically obese.
+
+# get the height in meters
+height = float(input ("Enter your height in meters: "))
+weight = float(input("Enter your weight in kg: "))
+
+# calculate the bmi
+bmi = int(weight / (height * height))
+print(bmi)
+message = ''
+#print messages
+if bmi < 18.5:
+    message =f" Your BMI is {bmi}, you are underweight"
+elif bmi >=18.5 and bmi < 25:
+    message =f" Your BMI is {bmi}, you have a normal weight"
+elif bmi >= 25 and bmi < 30:
+    message = f"Your BMI is {bmi}, you are slightly overweight"
+elif bmi >= 30 and bmi < 35:
+    message = f"Your BMI is {bmi}, you are overweight"
+else:
+    message =f" Your BMI is {bmi}, you are clininically obese"
+
+print(message)
