3bcf17dc5fb93dae3ac290cc2490d27957e9319c
Richard Asiedu <rich.learnonline@gmail.com>
2021-05-08 08:12:15 -0700

    day 2 concluded

--- day-2-tip-calculator.py
+++ day-2-tip-calculator.py
@@ -3,16 +3,17 @@ print("Welcome to the tip calculator 2.0")
 
 # Greet the user/ welcome message
 # ask for the total bill
-total_bill = int(input("What was the total bill: "))
+bill = float(input("What was the total bill: "))
 # ask for the percentage tip
-tip_percentage=int( input("What percentage tip would you like to give? 10, 12, 0r 15?: %"))
-tip_percent = tip_percentage /100;
-tip = tip_percent * total_bill
-new_bill = total_bill + tip
+tip =int( input("What percentage tip would you like to give? 10, 12, 0r 15?: %"))
+tip_percent = tip /100
+total_tip_amt = tip_percent * bill
+total_bill = bill + total_tip_amt
 # ask for the number of people to split tip 
-tip_shared_btn = int(input("How many people to split the bill?: "))
+people = int(input("How many people to split the bill?: "))
 # display each person bill to 2 dp
-each_person = new_bill /tip_shared_btn
-each_person_rounded = round(each_person, 2)
-final_tip =f"Each person should pay GHS{each_person_rounded}"
+bill_per_person = total_tip_amt /people
+final_amount = "{:.2f}".format(bill_per_person)
+# each_person_rounded = round(each_person, 2)
+final_tip =f"Each person should pay GHS{final_amount}"
 print(final_tip)
