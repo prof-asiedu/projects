5a88cc22284637429378e1414bd7db16eab15fd2
Richard Asiedu <rich.learnonline@gmail.com>
2021-05-09 07:19:13 -0700

    added the leap year file

--- day-3-leap-year.py
+++ day-3-leap-year.py
@@ -2,14 +2,13 @@
 year =int(input("Enter the year: " ))
 # check if input is a leap year
 if year % 4 == 0:
-    if year % 100 != 0:
-        print(" leap")
-    else:
+    if year % 100 == 0:
         if year % 400 == 0:
-            print("leap year")
-         
+           print("Leap year")
         else:
-            print("not leap year")
+            print("not leap year.")
+    else:
+        print("Leap year.")
 
 else:
     print("not leap")
